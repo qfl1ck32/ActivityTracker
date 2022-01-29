b2127a93b847cefaecd5b682d641500ebcac3196
qfl1ck32 <andrei.rusu@cultofcoders.com>
2022-01-29 21:34:43 +0200

    (add): ActivityLogsService

---
+++ microservices/api/src/bundles/AppBundle/services/ActivityLogs.service.ts
@@ -0,0 +1,48 @@
+import {
+  Service,
+  Inject,
+  EventManager,
+  ContainerInstance,
+} from "@bluelibs/core";
+import { ObjectId } from "@bluelibs/ejson";
+import { ActivityLogsCollection } from "../collections";
+import { EndUserService } from "./EndUser.service";
+import { EndUsersActivityLogsCreateInput } from "./inputs/EndUsersActivityLogsCreate.input";
+import { SecurityService } from "./Security.service";
+
+@Service()
+export class ActivityLogsService {
+  constructor(protected readonly container: ContainerInstance) {}
+
+  @Inject()
+  private securityService: SecurityService;
+
+  @Inject()
+  private activityLogsCollection: ActivityLogsCollection;
+
+  @Inject()
+  private endUserService: EndUserService;
+
+  public async create(
+    input: EndUsersActivityLogsCreateInput,
+    userId: ObjectId
+  ) {
+    const { name, activityId, noteModelId } = input;
+
+    const endUserId = await this.endUserService.getIdByOwnerId(userId);
+
+    await this.securityService.activityLogs.checkEndUserDoesNotHaveAnotherActivityLogForSameActivity(
+      activityId,
+      endUserId
+    );
+
+    const { insertedId } = await this.activityLogsCollection.insertOne({
+      activityId,
+      name,
+      endUserId,
+      noteModelId,
+    });
+
+    return insertedId;
+  }
+}
