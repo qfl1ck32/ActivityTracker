d125d20fda7a85a4008087c8cd16aca47098a31a
qfl1ck32 <andrei.rusu@cultofcoders.com>
2022-02-02 16:23:55 +0200

    (fix): jest config

--- microservices/api/jest.config.js
+++ microservices/api/jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   testEnvironment: "node",
 
   transform: { "\\.ts$": ["ts-jest"] },
+
+  moduleNameMapper: {
+    "@root/(.*)": "<rootDir>/src/$1",
+    "@bundles/(.*)": "<rootDir>/src/bundles/$1",
+  },
+
+  // TODO: https://stackoverflow.com/questions/50171412/jest-typescript-absolute-paths-baseurl-gives-error-cannot-find-module
 };
