87473a5918e1d25dcae38a0679d9afae548d84cc
qfl1ck32 <qfl1ck32@yahoo.com>
2022-02-07 06:27:19 +0200

    (fix): s3

--- microservices/api/src/bundles/AppBundle/__tests__/mock.test.ts
+++ microservices/api/src/bundles/AppBundle/__tests__/mock.test.ts
@@ -2,11 +2,10 @@ import { S3UploadService } from "@bluelibs/x-s3-bundle";
 import { container } from "@root/__tests__/ecosystem";
 import { mockImage } from "./utilities/files";
 
-// TODO: make sure it works; atm: ACLs error
 describe("test", () => {
-  //   test("idk", async () => {
-  //     const s3UploaService = container.get(S3UploadService);
-  //     const upload = await s3UploaService.upload(mockImage);
-  //     console.log(upload);
-  //   }, 50000);
+  // test("idk", async () => {
+  //   const s3UploaService = container.get(S3UploadService);
+  //   const upload = await s3UploaService.upload(mockImage);
+  //   console.log(upload);
+  // }, 50000);
 });
